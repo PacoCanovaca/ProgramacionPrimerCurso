bb5144e4c0993a4a8aac06d80825b68a74dd2d43
Paco <pacocanovaca97@gmail.com>
2025-10-07 19:34:16 +0200

    Primeros pasos

---
+++ ProyectoInicial/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    // Comentario de una línea
+        /* Comentario de
+        varias líneas
+         */
+    /**
+     * Definición de la clase
+     * @param args Esto es la información de entrada (parámetros y argumentos)
+     * @return type Esto es el retorno
+     * @version version
+     * @author nombre del autor
+     */
+    // TODO Tarea pendiente
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        String nombreCompleto = "Paco Canovaca";
+        int edad = 27;
+        edad = edad + 1;
+        System.out.println("Me llamo " + nombreCompleto);
+        System.out.println("Tengo " + edad + " años");
+
+        float altura = 1.75f;
+        double peso = 80.4385348582;
+
+        System.out.println("Mi altura es " + altura);
+        System.out.println("Peso " + peso + " kilos");
+
+        char letraCarnet = 'J';
+
+        System.out.println("La letra de mi DNI es " + letraCarnet);
+
+        boolean experiencia = false;
+
+        System.out.println("Experiencia: " + experiencia);
+    }
+
+}
