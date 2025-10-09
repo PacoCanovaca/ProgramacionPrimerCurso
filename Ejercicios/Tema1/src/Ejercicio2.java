a5b19a3f0ffe453a5d01ad3f194fa66ec7b8da66
Paco <pacocanovaca97@gmail.com>
2025-10-09 09:38:36 +0200

    Ejercicios 1 y 2 tema 1

---
+++ Ejercicios/Tema1/src/Ejercicio2.java
@@ -0,0 +1,23 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        // Modificando el valor de la variable puntuación
+        int puntuacion = 0;
+        System.out.println("Puntuación inicial: " + puntuacion);
+        puntuacion += 5;
+        System.out.println("Después de primera modificación: " + puntuacion);
+        puntuacion += 5;
+        System.out.println("Después de segunda modificación: " + puntuacion);
+        puntuacion += 5;
+        System.out.println("Puntuación final: " + puntuacion);
+
+        // Creando una variable para cada modificación de la puntuación
+        int puntuacion2 = 0;
+        int primeraModificacion = puntuacion + 5;
+        int segundaModificacion = primeraModificacion + 5;
+        int puntuacionFinal = segundaModificacion + 5;
+        System.out.println("Puntuación inicial: " + puntuacion2);
+        System.out.println("Después de primera modificación: " + primeraModificacion);
+        System.out.println("Después de segunda modificación: " + segundaModificacion);
+        System.out.println("Puntuación final: " + puntuacionFinal);
+    }
+}
