1477b1fa871cb06dca0f9baf3e43baaff538ebbc
Paco <pacocanovaca97@gmail.com>
2025-10-09 18:05:01 +0200

    Ejercicio 5 terminado

--- Ejercicios/Tema1/src/Ejercicio5.java
+++ Ejercicios/Tema1/src/Ejercicio5.java
@@ -3,7 +3,7 @@ public class Ejercicio5 {
         final String app = "MiApp";
         final String version = "1.0.0";
         final double pi = Math.PI;
-        final String currentUser;
+        final String currentUser = "Laura";
         final int level = 1;
         final int score = 0;
         final String updatedUser = "Miguel";
@@ -12,6 +12,12 @@ public class Ejercicio5 {
 
         System.out.println("Aplicación: " + app);
         System.out.println("Versión: " + version);
-        System.out.println("");
+        System.out.println("Valor de PI: " + pi);
+        System.out.println("Usuario actual: " + currentUser);
+        System.out.println("Nivel: " + level);
+        System.out.println("Puntuación: " + score);
+        System.out.println("Usuario actualizado: " + updatedUser);
+        System.out.println("Nivel actualizado: " + updatedLevel);
+        System.out.println("Puntuación actualizada: " + updatedScore);
     }
 }
