5c3618a56ab181f04d9064b5ab8e8c4bd774d795
Paco <pacocanovaca97@gmail.com>
2025-11-01 12:52:59 +0100

    Cambios en los apuntes de la sesión 28/10/2025

--- Lecciones/2025-10-28/src/Main.java
+++ Lecciones/2025-10-28/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
     static void main() {
-        sentenciaFor();
+//        sentenciaFor();
+        tablasMultiplicar();
 
 
     }
@@ -12,6 +13,15 @@ public class Main {
         }
         for (int i=100; i>=0; i-=10) {
             System.out.println(i);
+        }
+    }
+
+    public static void tablasMultiplicar() {
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Tabla del " + i);
+            for (int j = 0; j < 11; j++) {
+                System.out.printf("\t%d x %d = %d%n", i, j, i*j);
+            }
         }
     }
 
