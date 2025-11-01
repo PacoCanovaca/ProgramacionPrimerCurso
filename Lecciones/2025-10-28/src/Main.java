9e5e8de73f45a12f01e4911fc290e503b28455fc
Paco <pacocanovaca97@gmail.com>
2025-11-01 12:38:47 +0100

    Actualización del repositorio

---
+++ Lecciones/2025-10-28/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    static void main() {
+        sentenciaFor();
+
+
+    }
+
+    public static void sentenciaFor(){
+        for (int i=0; i<5; i++) {
+            System.out.println("Ejecutando línea");
+        }
+        for (int i=100; i>=0; i-=10) {
+            System.out.println(i);
+        }
+    }
+
+}
