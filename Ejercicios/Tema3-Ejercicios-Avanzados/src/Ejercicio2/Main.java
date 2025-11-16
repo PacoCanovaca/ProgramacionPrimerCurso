6c87d58532658cddb4c62d3d58877a9129645d03
Paco <pacocanovaca97@gmail.com>
2025-11-17 00:03:26 +0100

    Ejercicio 2 ejercicios avanzados Tema 3

--- Ejercicios/Tema3-Ejercicios-Avanzados/src/Ejercicio2/Main.java
+++ Ejercicios/Tema3-Ejercicios-Avanzados/src/Ejercicio2/Main.java
@@ -1,4 +1,9 @@
 package Ejercicio2;
 
 public class Main {
+    public static void main(String[] args) {
+        String secretWord = "LIBRO";
+        Wordle wordle = new Wordle();
+        wordle.repeatProcess(secretWord);
+    }
 }
--- Ejercicios/Tema3-Ejercicios-Avanzados/src/Ejercicio2/Wordle.java
+++ Ejercicios/Tema3-Ejercicios-Avanzados/src/Ejercicio2/Wordle.java
@@ -1,4 +1,54 @@
 package Ejercicio2;
 
+import java.util.Scanner;
+
 public class Wordle {
+    private void compareWord(String secretWord, String guess) {
+        int hits = 0;
+        int aprox = 0;
+        int misses = 0;
+        String correctLetters = "Los aciertos están en la/s posición/es ";
+        String aproxLetters = "Las aproximaciones están en la/s posición/es ";
+        for (int i = 0; i < secretWord.length(); i++) {
+            if (secretWord.charAt(i) == guess.charAt(i)) {
+                hits++;
+                correctLetters += i+1 + " ";
+            } else if (secretWord.contains(String.valueOf(guess.charAt(i)))) {
+                aprox++;
+                aproxLetters += i+1 + " ";
+            } else {
+                misses++;
+            }
+        }
+        System.out.printf("La palabra %s tiene:%n- Aciertos: %d%n- Aproximaciones: %d%n- Errores: %d%n", guess, hits, aprox, misses);
+        if (hits > 0) {
+            System.out.println(correctLetters);
+        }
+        if (aprox > 0) {
+            System.out.println(aproxLetters);
+        }
+    }
+
+    public void repeatProcess(String secretWord) {
+        Scanner scanner = new Scanner(System.in);
+        int tries = 0;
+        String word;
+        do {
+            System.out.print("Introduce tu palabra: ");
+            word = scanner.next().toUpperCase();
+            tries++;
+            if (secretWord.equals(word)) {
+                break;
+            }
+            compareWord(secretWord, word);
+            System.out.printf("Palabra errónea. Llevas %d intentos.%n", tries);
+        } while (tries < 5);
+        if (!word.equals(secretWord)) {
+            System.out.printf("Has perdido. La palabra correcta era %s", secretWord);
+        } else {
+            System.out.printf("¡Correcto! La palabra era %s. Has ganado el juego.", secretWord);
+        }
+        scanner.close();
+    }
+
 }
