ae5c1f6f1ae1a5a47a9c0912fe56c262df776253
Paco <pacocanovaca97@gmail.com>
2026-01-14 17:01:26 +0100

    Ejercicio 1 (por terminar) HashMap Tema 5 MPO

---
+++ Ejercicios/MPO/Tema5-HashMap/src/Ejercicio1/AgendaContactos.java
@@ -0,0 +1,53 @@
+package Ejercicio1;
+
+import java.util.HashMap;
+
+public class AgendaContactos {
+
+    private HashMap<String, String> agendaContactos = new HashMap<>();
+
+    public AgendaContactos(){}
+
+    public void agregarContacto(String nombre, String telefono) {
+        agendaContactos.put(nombre, telefono);
+        if (agendaContactos.get(nombre) == null) {
+            System.out.printf("Contacto añadido: %s - %s%n%n", nombre, telefono);
+        } else {
+            System.out.printf("Contacto actualizado: %s - %s%n%n", nombre, telefono);
+        }
+    }
+
+    public String buscarContacto(String nombre) {
+        return "El número de teléfono es " + agendaContactos.get(nombre) + "\n";
+    }
+
+    public boolean eliminarContacto(String nombre) {
+        if (agendaContactos.get(nombre) != null) {
+            agendaContactos.remove(nombre);
+            System.out.printf("Contacto %s eliminado%n%n", nombre);
+            return true;
+        }
+        System.out.printf("Contacto %s no encontrado%n%n", nombre);
+        return false;
+    }
+
+    public void listarContactos() {
+        for (String contacto: agendaContactos.keySet()) {
+            System.out.printf("%s - %s%n", contacto, agendaContactos.get(contacto));
+        }
+        System.out.println();
+    }
+
+    public boolean existeContacto(String nombre) {
+        if (agendaContactos.get(nombre) != null) {
+            System.out.println("El contacto existe\n");
+            return true;
+        } else {
+            System.out.println("El contacto no existe\n");
+            return false;
+        }
+    }
+
+
+
+}
