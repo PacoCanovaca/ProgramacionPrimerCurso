fd38e4a5942faa7557168886f6fc9caf2642aa47
Paco <pacocanovaca97@gmail.com>
2026-01-21 16:51:37 +0100

    Ejercicio 3 (terminado) HashMap Tema 5 MPO

--- Ejercicios/MPO/Tema5-HashMap/src/Ejercicio3/DiccionarioEspanolIngles.java
+++ Ejercicios/MPO/Tema5-HashMap/src/Ejercicio3/DiccionarioEspanolIngles.java
@@ -25,8 +25,55 @@ public class DiccionarioEspanolIngles {
         System.out.printf("Diccionario cargado con %d palabras.%n%n", traducciones.length);
     }
 
-    public void listarDiccionarios() {
-        
+    public int ordenarPalabras(String o1, String o2) {
+        for (int i = 0; i < o1.length(); i++) {
+            if (o1.charAt(i) > o2.charAt(i)) {
+                return 1;
+            } else if (o1.charAt(i) < o2.charAt(i)) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    public void listarDiccionario() {
+        System.out.println("Listando diccionario");
+        Set<String> palabras = diccionario.keySet();
+        ArrayList<String> palabrasEspanol = new ArrayList<>();
+        for (String palabra : palabras) {
+            palabrasEspanol.add(palabra);
+        }
+        Collections.sort(palabrasEspanol, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return ordenarPalabras(o1, o2);
+            }
+        });
+        for (String palabra : palabrasEspanol) {
+            String traduccion = diccionario.get(palabra);
+            System.out.printf("Traducción de %s: %s%n", palabra, traduccion);
+        }
+        System.out.println();
+    }
+
+    public int tamanioDiccionario() {
+        return diccionario.size();
+    }
+
+    public void traducirFrase(String frase) {
+        System.out.println("Traduciendo frase: " + frase);
+        String[] palabras = frase.split(" ");
+        String[] palabrasTraducidas = new String[palabras.length];
+        String fraseTraducida = "";
+        for (int i = 0; i < palabras.length; i++) {
+            palabrasTraducidas[i] = diccionario.get(palabras[i]);
+            if (i + 1 != palabras.length) {
+                fraseTraducida += palabrasTraducidas[i] + " ";
+            } else {
+                fraseTraducida += palabrasTraducidas[i];
+            }
+        }
+        System.out.println("Traducción: " + fraseTraducida + "\n");
     }
 
 }
--- Ejercicios/MPO/Tema5-HashMap/src/Ejercicio3/Main.java
+++ Ejercicios/MPO/Tema5-HashMap/src/Ejercicio3/Main.java
@@ -1,4 +1,20 @@
 package Ejercicio3;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        DiccionarioEspanolIngles diccionarioEspanolIngles = new DiccionarioEspanolIngles();
+
+        String[][] nuevasPalabras = new String[][]{{"hola", "hello"}, {"gato", "cat"}, {"casa", "house"}, {"el", "the"}, {"la", "the"}, {"en", "in"}, {"está", "is"}};
+
+        diccionarioEspanolIngles.agregarVariasTraducciones(nuevasPalabras);
+
+        diccionarioEspanolIngles.listarDiccionario();
+
+        diccionarioEspanolIngles.traducirFrase("el gato está en la casa");
+
+        System.out.println("Total de palabras del diccionario: " + diccionarioEspanolIngles.tamanioDiccionario());
+    }
+
 }
