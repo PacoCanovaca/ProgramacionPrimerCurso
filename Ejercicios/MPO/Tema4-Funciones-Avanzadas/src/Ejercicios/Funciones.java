ac3f2e96b3bd17f12897b5f0232a2ea5a7ed647c
Paco <pacocanovaca97@gmail.com>
2026-02-03 14:35:45 +0100

    Ejercicios 6-9 Funciones Avanzadas Tema 4 MPO

--- Ejercicios/MPO/Tema4-Funciones-Avanzadas/src/Ejercicios/Funciones.java
+++ Ejercicios/MPO/Tema4-Funciones-Avanzadas/src/Ejercicios/Funciones.java
@@ -1,5 +1,7 @@
 package Ejercicios;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.function.*;
 
 public class Funciones {
@@ -16,5 +18,30 @@ public class Funciones {
     public BiPredicate<String, String> tieneMismaLongitud = (p1, p2) -> p1.length() == p2.length();
 
     public BiConsumer<String, Integer> impresion = (nombre, edad) -> System.out.printf("Nombre: %s, Edad: %d%n", nombre, edad);
+
+    public Function<Integer, String> conversion = n -> Integer.toString(n);
+    public Function<Integer, Integer> sumaDiez = n -> n+10;
+    public Function<Integer, Integer> multiplicaDos = n -> n*2;
+
+    public BiFunction<Producto, Producto, Producto> buscarMasCaro = (p1, p2) -> p1.getPrecio()>p2.getPrecio() ? p1 : p2;
+
+    public Predicate<Integer> mayorQue50 = n -> n > 50;
+    public Predicate<Integer> divisiblePor3 = n -> n % 3 == 0;
+
+    public BiFunction<ArrayList<Integer>, ArrayList<Integer>, ArrayList<Integer>> unirListas = (x, y) -> {
+        ArrayList<Integer> nuevaLista = new ArrayList<>();
+        for (int numero : x) {
+            if (!nuevaLista.contains(numero)) {
+                nuevaLista.add(numero);
+            }
+        }
+        for (int numero : y) {
+            if (!nuevaLista.contains(numero)) {
+                nuevaLista.add(numero);
+            }
+        }
+        Collections.sort(nuevaLista);
+        return nuevaLista;
+    };
 
 }
--- Ejercicios/MPO/Tema4-Funciones-Avanzadas/src/Main.java
+++ Ejercicios/MPO/Tema4-Funciones-Avanzadas/src/Main.java
@@ -1,4 +1,5 @@
 import Ejercicios.Funciones;
+import Ejercicios.Producto;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -50,6 +51,78 @@ public class Main {
         funciones.impresion.accept("Paco", 28);
         */
 
+        /* Ejercicio 6
+        System.out.println(funciones.sumaDiez.andThen(funciones.multiplicaDos.andThen(funciones.conversion)).apply(2));
+         */
+
+        /* Ejercicio 7
+        Producto producto1 = new Producto("Teclado", 80);
+        Producto producto2 = new Producto("Ratón", 40);
+        Producto producto3 = new Producto("Pantalla", 200);
+        Producto producto4 = new Producto("Ordenador", 1400);
+        Producto producto5 = new Producto("Pendrive", 5);
+        Producto productoCaro = funciones.buscarMasCaro.apply(producto1, producto2);
+        System.out.println("Producto más caro:" + productoCaro.getNombre());
+
+        ArrayList<Producto> listaProductos = new ArrayList<>();
+        listaProductos.add(producto1);
+        listaProductos.add(producto2);
+        listaProductos.add(producto3);
+        listaProductos.add(producto4);
+        listaProductos.add(producto5);
+        Producto productoMasCaro = listaProductos.get(0);
+        for (int i = 0; i < (listaProductos.size() - 1); i++) {
+            productoMasCaro = funciones.buscarMasCaro.apply(listaProductos.get(i), listaProductos.get(i+1));
+        }
+        System.out.println("Producto más caro:" + productoMasCaro.getNombre());
+         */
+
+        /* Ejercicio 8
+        ArrayList<Integer> numeros = new ArrayList<>();
+        System.out.print("Lista original:");
+        for (int i = 0; i < 10; i++) {
+            numeros.add((int) (Math.random() * 100));
+            System.out.print(" " + numeros.get(i));
+        }
+        System.out.print("\nLista de pares, divisibles por 3 y mayores que 50:");
+        numeros.forEach(item -> {
+            if (funciones.esPar.and(funciones.divisiblePor3.and(funciones.mayorQue50)).test(item)) {
+                System.out.print(" " + item);
+            }
+        });
+        System.out.print("\nLista de pares o divisibles por 3 o mayores que 50:");
+        numeros.forEach(item -> {
+            if (funciones.esPar.or(funciones.divisiblePor3.or(funciones.mayorQue50)).test(item)) {
+                System.out.print(" " + item);
+            }
+        });
+        System.out.print("\nLista de impares, indivisibles por 3 y menores que 50:");
+        numeros.forEach(item -> {
+            if (funciones.esPar.negate().and(funciones.divisiblePor3.negate().and(funciones.mayorQue50.negate())).test(item)) {
+                System.out.print(" " + item);
+            }
+        });
+         */
+
+        ArrayList<Integer> numeros1 = new ArrayList<>();
+        System.out.print("Lista 1:");
+        for (int i = 0; i < 10; i++) {
+            numeros1.add((int) (Math.random() * 100));
+            System.out.print(" " + numeros1.get(i));
+        }
+        System.out.println();
+        ArrayList<Integer> numeros2 = new ArrayList<>();
+        System.out.print("Lista 2:");
+        for (int i = 0; i < 10; i++) {
+            numeros2.add((int) (Math.random() * 100));
+            System.out.print(" " + numeros2.get(i));
+        }
+        System.out.println();
+        ArrayList<Integer> listaFinal = funciones.unirListas.apply(numeros1, numeros2);
+        System.out.print("Lista final:");
+        for (int i = 0; i < listaFinal.size(); i++) {
+            System.out.print(" " + listaFinal.get(i));
+        }
 
     }
 
